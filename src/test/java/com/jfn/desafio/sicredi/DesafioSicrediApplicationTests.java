93cbf7c00ec1b08bc2f52b15f48bb59364911655
Jaime Fernando Neis <jaimefneis@gmail.com>
2020-10-02 18:46:53 -0300

    Projeto Spring criado

---
+++ src/test/java/com/jfn/desafio/sicredi/DesafioSicrediApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfn.desafio.sicredi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSicrediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
